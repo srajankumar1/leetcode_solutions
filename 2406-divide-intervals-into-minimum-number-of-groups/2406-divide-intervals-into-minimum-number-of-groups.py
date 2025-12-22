001aafdb1ff05745c6873d0672e49444ad5641ef
srajankumar1 <srajan.ai24@bmsce.ac.in>
2025-12-22 22:41:09 +0530

    Time: 471 ms (45.59%), Space: 37.5 MB (88.24%) - LeetHub

---
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
@@ -0,0 +1,13 @@
+import heapq
+
+class Solution:
+    def minGroups(self,intervals):
+        intervals.sort()
+        heap=[]
+
+        for start,end in intervals:
+            if heap and heap[0]<start:
+                heapq.heappop(heap)
+            heapq.heappush(heap,end)
+
+        return len(heap)
