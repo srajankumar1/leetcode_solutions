0c5ebb007eb62a58b04b81262e6a95821fb767c1
Srajan <srajan.ai24@bmsce.ac.in>
2026-02-24 19:37:05 +0530

    Time: 4 ms (50.52%), Space: 12.7 MB (55.88%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,12 +1,23 @@
 class Solution:
     def findRedundantConnection(self, edges):
         parent=[i for i in range(len(edges)+1)]
+        rank=[0]*(len(edges)+1)
         def find(x):
             if parent[x]!=x:
                 parent[x]=find(parent[x])
             return parent[x]
+        def union(x,y):
+            px,py=find(x),find(y)
+            if px==py:
+                return False
+            if rank[px]<rank[py]:
+                parent[px]=py
+            elif rank[px]>rank[py]:
+                parent[py]=px
+            else:
+                parent[py]=px
+                rank[px]+=1
+            return True
         for u,v in edges:
-            pu,pv=find(u),find(v)
-            if pu==pv:
+            if not union(u,v):
                 return [u,v]
-            parent[pu]=pv
