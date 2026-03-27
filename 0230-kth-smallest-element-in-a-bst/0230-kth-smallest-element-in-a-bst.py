5dcfbd582895a712153ea00e60232112135a6978
Srajan <srajan.ai24@bmsce.ac.in>
2026-03-27 22:15:53 +0530

    Time: 0 ms (100%), Space: 20.2 MB (55.45%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -1,12 +1,13 @@
-class Solution:
+class Solution(object):
     def kthSmallest(self, root, k):
         stack=[]
         while True:
             while root:
                 stack.append(root)
                 root=root.left
+
             root=stack.pop()
             k-=1
             if k==0:
                 return root.val
-            root=root.right
+            root=root.right 
