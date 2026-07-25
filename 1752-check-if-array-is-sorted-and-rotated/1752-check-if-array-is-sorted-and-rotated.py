d3cba0f8b0ae4df3c74af713f11547b2bf0c2f5d
Srajan <srajan.ai24@bmsce.ac.in>
2026-07-25 10:39:16 +0530

    Time: 0 ms (100%), Space: 12.5 MB (18.12%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def check(self, nums):
+        n=len(nums)
+        count=0
+        for i in range(n):
+            if nums[i]>nums[(i+1)%n]:
+                count+=1
+        return count<=1
