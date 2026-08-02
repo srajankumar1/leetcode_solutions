187208c04c6480dc05ea8c633cfb88518698148c
Srajan <srajan.ai24@bmsce.ac.in>
2026-08-02 22:10:17 +0530

    Time: 7 ms (26.3%), Space: 19.4 MB (22.27%) - LeetHub

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def removeElements(self, head, val):
+
+        dummy = ListNode(0)
+        dummy.next = head
+
+        prev = dummy
+        curr = head
+
+        while curr:
+
+            if curr.val == val:
+                prev.next = curr.next
+            else:
+                prev = curr
+
+            curr = curr.next
+
+        return dummy.next
