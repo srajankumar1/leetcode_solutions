a40a2ed6aef29807538e8d53f767dfede61de7be
Srajan <srajan.ai24@bmsce.ac.in>
2026-08-22 11:33:55 +0530

    Time: 0 ms (100%), Space: 12.6 MB (16.28%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,28 +1,26 @@
 class Solution(object):
     def compress(self, chars):
-        count = 1
-        s = []
+        count=1
+        s=[]
 
         s.append(chars[0])
 
-        for i in range(1, len(chars)):
-            if chars[i - 1] != chars[i]:
-                if count > 1:
+        for i in range(1,len(chars)):
+            if chars[i-1]!=chars[i]:
+                if count>1:
                     for digit in str(count):
                         s.append(digit)
 
                 s.append(chars[i])
-                count = 1
+                count=1
             else:
-                count += 1
+                count+=1
 
-        # Handle the last group
-        if count > 1:
+        if count>1:
             for digit in str(count):
                 s.append(digit)
 
-        # Modify chars in-place
         for i in range(len(s)):
-            chars[i] = s[i]
+            chars[i]=s[i]
 
         return len(s)
