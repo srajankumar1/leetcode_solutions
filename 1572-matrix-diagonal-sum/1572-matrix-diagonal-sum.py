9464d42d9fcc4564e460d5ddeb939cdf27fdec03
Srajan <srajan.ai24@bmsce.ac.in>
2026-08-25 18:38:35 +0530

    Time: 4 ms (12.79%), Space: 12.5 MB (77.9%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def diagonalSum(self, mat):
+        sum=0
+        for i in range(len(mat)):
+            for j in range(len(mat[i])):
+                if i==j or i+j==(len(mat)-1):
+                    sum+=mat[i][j]
+        return sum
